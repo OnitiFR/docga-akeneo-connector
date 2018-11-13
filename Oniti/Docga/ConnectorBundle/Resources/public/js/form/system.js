f5c2c3446fd3d64192eec9c9bc68c72874a99998
Francois BOUYER <francois@oniti.fr>
2018-11-13 22:37:57 +0100

    correction divers

---
+++ Oniti/Docga/ConnectorBundle/Resources/public/js/form/system.js
@@ -0,0 +1,57 @@
+/*
+ * This file is part of the Ephoto Connector Bundle package.
+ */
+
+"use strict";
+
+define([
+        'underscore',
+        'oro/translator',
+        'routing',
+        'pim/form',
+        'text!onitidocgaconnector/templates/system/group/configuration.html'
+    ],
+    function (_,
+              __,
+              Routing,
+              BaseForm,
+              template) {
+        return BaseForm.extend({
+            className: 'AknFormContainer AknFormContainer--withPadding',
+            events: {
+                'change .ephoto-config': 'updateModel'
+            },
+            isGroup: true,
+            label: __('docga.configuration.tab'),
+            template: _.template(template),
+
+            render: function () {
+                this.$el.html(this.template({
+                    base_url: this.getFormData()['pim_oniti_docga_connector___base_url'] ?
+                        this.getFormData()['pim_oniti_docga_connector___base_url'].value : '',
+                    base_url: this.getFormData()['pim_oniti_docga_connector___api_key'] ?
+                        this.getFormData()['pim_oniti_docga_connector___api_key'].value : '',
+                    base_url: this.getFormData()['pim_oniti_docga_connector___api_secret'] ?
+                        this.getFormData()['pim_oniti_docga_connector___api_secret'].value : '',
+                }));
+
+
+                this.delegateEvents();
+
+                return BaseForm.prototype.render.apply(this, arguments);
+            },
+
+            updateModel: function (event) {
+                var name = event.target.name;
+                var data = this.getFormData();
+                var newValue = event.target.value;
+                if (name in data) {
+                    data[name].value = newValue;
+                } else {
+                    data[name] = {value: newValue};
+                }
+                this.setData(data);
+            }
+        });
+    }
+);
